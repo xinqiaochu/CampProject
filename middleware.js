ba721b8ff5527ad468b0c821d7fbec67760d8d20
xinqiaochu <54684989+xinqiaochu@users.noreply.github.com>
2021-12-10 15:27:05 -0800

    Authorization

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
-        req.flash('error', 'You must be signed in');
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'You must be signed in!');
         res.redirect('/login');
     }
     next();
