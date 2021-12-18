eb88739a25e8205ea781c5dd7e8f1fd16203a219
xinqiaochu <54684989+xinqiaochu@users.noreply.github.com>
2021-12-17 17:34:28 -0800

    mapbox install

--- models/campground.js
+++ models/campground.js
@@ -16,6 +16,18 @@ ImageSchema.virtual('thumbnail').get(function(){
 const campgroundSchema = new Schema({
     title : String,
     images : [ImageSchema],
+    geometry:{
+        type:{
+            type: String,
+            enum:['Point'],
+            required: true
+        },
+        coordinates:{
+            type:[Number],
+            required: true
+        }
+
+    },
     price : Number,
     description : String,
     location : String,
